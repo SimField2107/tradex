2144ed2a1ce0918cb9b7c91aedbc0b5c9ab03f7d
Seline Cornfield <Seline2107@googlemail.com>
2025-09-21 14:16:24 +0100

    fix(data): Replace Avalanche with Polygon in mock assets

--- src/pages/api/stakeable-assets.ts
+++ src/pages/api/stakeable-assets.ts
@@ -12,7 +12,7 @@ const stakeableAssets: StakeableAsset[] = [
   { id: 'ethereum', name: 'Ethereum', symbol: 'ETH', image: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png', apy: 4.5 },
   { id: 'solana', name: 'Solana', symbol: 'SOL', image: 'https://assets.coingecko.com/coins/images/4128/large/solana.png', apy: 7.2 },
   { id: 'cardano', name: 'Cardano', symbol: 'ADA', image: 'https://assets.coingecko.com/coins/images/975/large/cardano.png', apy: 3.8 },
-  { id: 'avalanche-2', name: 'Avalanche', symbol: 'AVAX', image: 'https://assets.coingecko.com/coins/images/12559/large/avalanche-avax-logo.png', apy: 8.1 },
+  { id: 'matic-network', name: 'Polygon', symbol: 'MATIC', image: 'https://assets.coingecko.com/coins/images/4713/standard/matic-token-icon.png', apy: 6.5 },
 ];
 
 export default function handler(
